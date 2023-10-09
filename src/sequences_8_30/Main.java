c9caa4b9352f01a45887b438ef4a7e14a2297413
trojkat <michal.stukow@ug.edu.pl>
2023-10-09 10:02:03 +0200

    Zakończone wyświetlanie liczb w kolumnach

--- src/sequences_8_30/Main.java
+++ src/sequences_8_30/Main.java
@@ -10,11 +10,37 @@ package sequences_8_30;
  */
 public class Main {
 
+    final static int MAX = 50;
+    final static int MAX_IN_LINE = 24;
+    final static int MAX_LENGTH=getNumberOfDigits(MAX);
+
+    private static int getNumberOfDigits(int n){
+        int nd=0;
+        while(n>0){
+            nd++;
+            n=(n-(n % 10))/10;
+        }
+        return nd;
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i < MAX; i++) {
+            System.out.print(i);
+            if (i < MAX - 1) {
+                System.out.print(", ");
+            }
+            if (getNumberOfDigits(i)<MAX_LENGTH){
+                for (int j=0;j<MAX_LENGTH-getNumberOfDigits(i);j++){
+                    System.out.print(" ");
+                }
+            }
+            if (i % MAX_IN_LINE == 0) {
+                System.out.print("\n");
+            }
+        }
+        System.out.print("\n");
     }
-    
 }
