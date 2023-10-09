f6d5a7b7b1458f22d5bc8717b6b09465fc03d286
trojkat <michal.stukow@ug.edu.pl>
2023-10-09 09:11:13 +0200

    The first commit

---
+++ src/sequences_8_30/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sequences_8_30;
+
+/**
+ *
+ * @author xenon
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    
+}
