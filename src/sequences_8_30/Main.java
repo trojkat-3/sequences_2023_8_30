e21e8db9dcfda8730a320df8c91d31eb565e44fc
trojkat <michal.stukow@ug.edu.pl>
2023-10-16 10:02:26 +0200

    Zakończone zapisywanie do pliku

--- src/sequences_8_30/Main.java
+++ src/sequences_8_30/Main.java
@@ -1,4 +1,3 @@
-
 package sequences_8_30;
 
 import java.io.FileWriter;
@@ -12,47 +11,42 @@ public class Main {
 
     final static int MAX = 200;
     final static int MAX_IN_LINE = 16;
-    final static int MAX_LENGTH=getNumberOfDigits(MAX);
+    final static int MAX_LENGTH = getNumberOfDigits(MAX);
 
-    private static int getNumberOfDigits(int n){
-        int nd=0;
-        while(n>0){
+    private static int getNumberOfDigits(int n) {
+        int nd = 0;
+        while (n > 0) {
             nd++;
-            n=(n-(n % 10))/10;
+            n = (n - (n % 10)) / 10;
         }
         return nd;
     }
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        
-        FileWriter myWriter=null;
-        try {
-               myWriter = new FileWriter("output/integers.txt");  
+
+        try (FileWriter myWriter = new FileWriter("output/integers.txt");) {
             //myWriter.write("Files in Java might be tricky, but it is fun enough!");
             //myWriter.close();
-        } catch(IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-      
-        
-        for (int i = 1; i < MAX; i++) {
-            myWriter.write(Integer.toString(i));
-            if (i < MAX - 1) {
-                myWriter.write(", ");
-            }
-            if (getNumberOfDigits(i)<MAX_LENGTH){
-                for (int j=0;j<MAX_LENGTH-getNumberOfDigits(i);j++){
-                    myWriter.write(" ");
+            for (int i = 1; i < MAX; i++) {
+                myWriter.write(Integer.toString(i));
+                if (i < MAX - 1) {
+                    myWriter.write(", ");
+                }
+                if (getNumberOfDigits(i) < MAX_LENGTH) {
+                    for (int j = 0; j < MAX_LENGTH - getNumberOfDigits(i); j++) {
+                        myWriter.write(" ");
+                    }
+                }
+                if (i % MAX_IN_LINE == 0) {
+                    myWriter.write("\n");
                 }
             }
-            if (i % MAX_IN_LINE == 0) {
-                myWriter.write("\n");
-            }
+            myWriter.write("\n");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
         }
-        myWriter.write("\n");
-        myWriter.close();
     }
 }
