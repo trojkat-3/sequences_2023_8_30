8c3575a7af7c1d0893f85fa8cea01ec5211bff99
trojkat <michal.stukow@ug.edu.pl>
2023-11-13 10:01:06 +0100

    Added new Squences subclasses - continued

--- src/sequences_8_30/Main.java
+++ src/sequences_8_30/Main.java
@@ -1,41 +1,26 @@
 package sequences_8_30;
 
-import java.util.ArrayList;
 import sequences_8_30.print.IntegersPrinter;
+import sequences_8_30.sequence.Fibonacci;
 import sequences_8_30.sequence.Integers;
+import sequences_8_30.sequence.Primes;
 import sequences_8_30.sequence.Sequence;
+import sequences_8_30.sequence.Squares;
 
-public class Main {
-
-    
-
+public class Main { 
    
-
-   
-    
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {    
+      public static void main(String[] args) {    
         IntegersPrinter printerStdOut = new IntegersPrinter();
         IntegersPrinter printerFile = new IntegersPrinter("integers.txt");
-   
-        //integers
         
-        printerStdOut.print(list, "Integers");
-        //squares
-        list.clear();
-        
-        printerFile.print(list, "Squares");
-        //primes
-        list.clear();
-        
-        printerFile.print(list, "Primes");
-        //Fibonacci
-        list.clear();
-        
-        printerStdOut.print(list,"Fibonacci numbers");
-        printerFile.print(list,"Fibonacci numbers"); 
+        Sequence sq=new Integers();
+        //printerStdOut.print(listToOutput, sq.getName());
+        //print
+        sq=new Squares();
+        //print
+        sq=new Primes();
+        //print
+        sq=new Fibonacci();
+        //print
     }
 }
